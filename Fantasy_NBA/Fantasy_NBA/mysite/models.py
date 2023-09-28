bd5b6dd36a11c5763b36784ed085ff5656d6622b
Muhibur Rahman <mrmuhibur47@gmail.com>
2023-09-28 19:22:38 +0100

    Update models.py

--- Fantasy_NBA/Fantasy_NBA/mysite/models.py
+++ Fantasy_NBA/Fantasy_NBA/mysite/models.py
@@ -6,11 +6,18 @@ class NBAPlayer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     team = models.CharField(max_length=50, default='Unknown')
-    # Add more fields as needed (e.g., height, weight, position, etc.)
 
-    points_per_game = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    assists_per_game = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    rebounds_per_game = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    PPG = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    APG = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    RPG = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    SPG = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    BPG = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    FG_PCT = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    FG3_PCT = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    FT_PCT = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    MIN = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    TOV = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
