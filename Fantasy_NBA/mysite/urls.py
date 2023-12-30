5e961d92ddf85b65f28b5b5b113faeb7f686296f
Muhibur Rahman <mrmuhibur47@gmail.com>
2023-12-30 00:34:45 +0000

    created a player comparison page

--- Fantasy_NBA/mysite/urls.py
+++ Fantasy_NBA/mysite/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-path("", login_required(views.home), name="home")
+path("", login_required(views.home), name="home"),
+path('compare/<str:FN1>/<str:FN2>/', login_required(views.playercomparison), name="playercomparison")
 ]
--- Fantasy_NBA/mysite/views.py
+++ Fantasy_NBA/mysite/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import NBAPlayer
+import json
+from django.core.serializers import serialize
 
 
 # Create your views here.
@@ -8,3 +11,26 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(response):
     return render(response, "mysite/home.html", {})
+
+@login_required
+def playercomparison(response, FN1, FN2):
+    player1 = NBAPlayer.objects.get(first_name=FN1)
+    player2 = NBAPlayer.objects.get(first_name=FN2)
+
+    player1_stats = {
+        'PPG': player1.PPG,
+        'APG': player1.APG,
+        'RPG': player1.RPG,
+        'SPG': player1.SPG,
+        'BPG': player1.BPG,
+    }
+
+    player2_stats = {
+        'PPG': player2.PPG,
+        'APG': player2.APG,
+        'RPG': player2.RPG,
+        'SPG': player2.SPG,
+        'BPG': player2.BPG,
+    }
+    
+    return render(response, 'mysite/playercomparison.html', {'player1_stats': player1_stats, 'player2_stats': player2_stats})
