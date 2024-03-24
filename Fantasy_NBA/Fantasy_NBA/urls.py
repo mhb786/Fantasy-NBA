f45e2eb61b80444d3c3f92fa119c251e4d72827f
Muhibur Rahman <mrmuhibur47@gmail.com>
2024-03-24 13:13:04 +0000

    complete password reset functionality

--- Fantasy_NBA/Fantasy_NBA/urls.py
+++ Fantasy_NBA/Fantasy_NBA/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 from register import views as v
 
 urlpatterns = [
@@ -23,4 +26,24 @@ urlpatterns = [
     path('register/', v.register, name='register'),
     path("", include("mysite.urls")),
     path("", include("django.contrib.auth.urls")),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='registration/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
